de3772f9d535f9b10cc208ecc402821306ba24ff
Nandhinipriya <nandhinipriya745@gmail.com>
2022-10-21 09:31:50 +0530

    Add project files.

---
+++ JenkinsDemo2/JenkinsDemo2/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsDemo2
+{
+    public class CalculatorLibJenkinsDemp
+    {
+        int i, j,ans;
+        public void Add()
+        {
+            ans = i + j;
+        }
+        public void Subtract()
+        {
+            ans = i - j;
+        }
+        public void Multiply()
+        {
+            ans = i * j;
+        }
+
+    }
+}
